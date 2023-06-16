b33fc4ffa34fd7af9d8a948ffff13991bfa425c8
Mahmud Ridwan <m@hjr265.me>
2023-06-16 11:45:05 +0600

    Print banner

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/FurqanSoftware/pog"
@@ -10,13 +11,13 @@ func main() {
 	log.SetFlags(0)
 	pog.InitDefault()
 
-	log.Println(` ______       _ _      _   `)
-	log.Println(` | ___ \     | | |    | |  `)
-	log.Println(` | |_/ /_   _| | | ___| |_ `)
-	log.Println(` | ___ \ | | | | |/ _ \ __|`)
-	log.Println(` | |_/ / |_| | | |  __/ |_ `)
-	log.Println(` \____/ \__,_|_|_|\___|\__|`)
-	log.Println()
+	fmt.Fprintln(log.Writer(), ` ______       _ _      _   `)
+	fmt.Fprintln(log.Writer(), ` | ___ \     | | |    | |  `)
+	fmt.Fprintln(log.Writer(), ` | |_/ /_   _| | | ___| |_ `)
+	fmt.Fprintln(log.Writer(), ` | ___ \ | | | | |/ _ \ __|`)
+	fmt.Fprintln(log.Writer(), ` | |_/ / |_| | | |  __/ |_ `)
+	fmt.Fprintln(log.Writer(), ` \____/ \__,_|_|_|\___|\__|`)
+	fmt.Fprintln(log.Writer())
 
 	err := RootCmd.Execute()
 	if err != nil {
