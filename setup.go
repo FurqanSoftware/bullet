3f4f95c8bfa0f492b4951b1e017e14a4f55acae7
Mahmud Ridwan <m@hjr265.me>
2025-08-15 12:36:56 +0600

    Update setup.go

--- setup.go
+++ setup.go
@@ -5,17 +5,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	flagSetupEnviron string
+)
+
 var SetupCmd = &cobra.Command{
 	Use:   "setup",
 	Short: "Setup server for application",
 	Long:  `This command prepares the server for the application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		environ, _ := cmd.Flags().GetString("environ")
-		return core.Setup(currentScope, currentConfiguration, environ)
+		return core.Setup(currentScope, currentConfiguration, flagSetupEnviron)
 	},
 }
 
 func init() {
-	SetupCmd.Flags().String("environ", "", "if set, push file as environment")
+	SetupCmd.Flags().StringVarP(&flagSetupEnviron, "environ", "", "", "if set, push file as environment")
 	RootCmd.AddCommand(SetupCmd)
 }
