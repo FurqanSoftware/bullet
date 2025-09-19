d6306f06b486ff4c22e61efc70fc0ae716ec7a7a
Mahmud Ridwan <m@hjr265.me>
2025-09-19 14:21:50 +0600

    Allow multiple nodes in environ push selection

--- environ.go
+++ environ.go
@@ -10,7 +10,7 @@ var EnvironPushCmd = &cobra.Command{
 	Short: "Push application environment from file to server",
 	Long:  `This command configures the application in the server based on specific environment file.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		s := NewSelector().Node(currentScope)
+		s := NewSelector().Nodes(currentScope)
 		return core.EnvironPush(s, currentConfiguration, args[0])
 	},
 }
